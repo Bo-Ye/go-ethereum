6cd52eab01a10a8899bfbdcfc812ec0e1d64d2c1
Bo Ye <boy.e.computer.1982@outlook.com>
2017-08-07 23:04:09 +1200

    add TestLDBDatabaseMeter testing

--- ethdb/database_test.go
+++ ethdb/database_test.go
@@ -21,11 +21,15 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
+	"github.com/ethereum/go-ethereum/metrics"
 )
 
 var (
-	key   = []byte("key-abc")
-	value = []byte("value-bcd")
+	key1   = []byte("key-aaaaa")
+	value1 = []byte("value-aaaaa")
+	key2 = []byte("key-bbbbb")
+	value2 = []byte("value-bbbbb")
+	key3 = []byte("unknow")
 )
 
 //test LDBDatabase
@@ -39,19 +43,19 @@ func TestLDBDatabase(t *testing.T) {
 		t.Errorf("expected %s got %s", tmpDir, dbPath)
 	}
 	//test Put
-	db.Put(key, value)
-	ret1, _ := db.db.Has(key, nil)
+	db.Put(key1, value1)
+	ret1, _ := db.db.Has(key1, nil)
 	if ret1 != true {
 		t.Errorf("expected %t got %t", true, ret1)
 	}
 	//test Get
-	ret2, _ := db.Get(key)
-	if !bytes.Equal(ret2, value) {
-		t.Errorf("expected %x got %x", value, ret2)
+	ret2, _ := db.Get(key1)
+	if !bytes.Equal(ret2, value1) {
+		t.Errorf("expected %x got %x", value1, ret2)
 	}
 	//test Delete
-	db.Delete(key)
-	ret3, _ := db.db.Has(key, nil)
+	db.Delete(key1)
+	ret3, _ := db.db.Has(key1, nil)
 	if ret3 {
 		t.Errorf("expected %t got %t", false, ret3)
 	}
@@ -61,7 +65,58 @@ func TestLDBDatabaseMeter(t *testing.T) {
 	tmpDir, _ := ioutil.TempDir("", "ldbtesttmpdir")
 	defer os.RemoveAll(tmpDir)
 	db, _ := NewLDBDatabase(tmpDir, 0, 0)
+	defer db.Close()
+	//enable metrics
+	metrics.Enabled = true
 	db.Meter("prefix")
+	//test putTimer && writeMeter
+	db.Put(key1, value1)
+	db.Put(key2, value2)
+	ret := db.putTimer.Count()
+	if ret != 2{
+		t.Errorf("putTimer: expected %d got %d", 2, ret)
+	}
+	min := db.putTimer.Min()
+	if min == 0{
+		t.Error("putTimer: expected min time larger than zero got 0")
+	}
+	mean := db.putTimer.Mean()
+	if mean == 0{
+		t.Error("putTimer: expected mean time larger than zero got 0")
+	}
+	max := db.putTimer.Max()
+	if max == 0{
+		t.Error("putTimer: expected max time larger than zero got 0")
+	}
+	totalBytes := int64(len(value1) + len(value2))
+	ret = db.writeMeter.Count()
+	if totalBytes != ret {
+		t.Errorf("writeMeter: expected %d got %d", totalBytes, ret)
+	}
+	//test getTimer & missMeter
+	db.Get(key1)
+	db.Get(key3)
+	ret = db.getTimer.Count()
+	if ret != 2{
+		t.Errorf("getTimer: expected %d got %d", 2, ret)
+	}
+	min = db.getTimer.Min()
+	if min == 0{
+		t.Error("getTimer: expected min time larger than zero got 0")
+	}
+	mean = db.getTimer.Mean()
+	if mean == 0{
+		t.Error("getTimer: expected mean time larger than zero got 0")
+	}
+	max = db.getTimer.Max()
+	if max == 0{
+		t.Error("getTimer: expected max time larger than zero got 0")
+	}
+	ret = db.missMeter.Count()
+	if ret != 1{
+		t.Errorf("missMeter: expected %d got %d", 1, ret)
+	}
+
 }
 
 //////test table
@@ -71,19 +126,19 @@ func TestTable(t *testing.T) {
 	db, _ := NewLDBDatabase(tmpDir, 0, 0)
 	table := NewTable(db, "prefix-")
 	//test put
-	table.Put(key, value)
-	ret1, _ := db.db.Has(append([]byte("prefix-"), key...), nil)
+	table.Put(key1, value1)
+	ret1, _ := db.db.Has(append([]byte("prefix-"), key1...), nil)
 	if !ret1 {
 		t.Errorf("expected %t got %t", true, ret1)
 	}
 	//test get
-	ret2, _ := table.Get(key)
-	if !bytes.Equal(ret2, value) {
-		t.Errorf("expected %s got %s", value, ret2)
+	ret2, _ := table.Get(key1)
+	if !bytes.Equal(ret2, value1) {
+		t.Errorf("expected %s got %s", value1, ret2)
 	}
 	//test delete
-	table.Delete(key)
-	ret3, _ := db.db.Has(append([]byte("prefix-"), key...), nil)
+	table.Delete(key1)
+	ret3, _ := db.db.Has(append([]byte("prefix-"), key1...), nil)
 	if ret3 {
 		t.Errorf("expected %t got %t", false, ret3)
 	}
